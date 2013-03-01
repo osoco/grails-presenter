615de0eb503b297434ed4b6bb1f380b7a01a9061
Marcin Gryszko <marcin@grysz.com>
2013-03-01 13:46:03 +0100

    Minor formatting

--- src/groovy/es/osoco/grails/plugins/presenter/ClassNameAsBeanNameGenerator.groovy
+++ src/groovy/es/osoco/grails/plugins/presenter/ClassNameAsBeanNameGenerator.groovy
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.beans.factory.support.BeanDefinitionRegistry
 import org.springframework.beans.factory.support.BeanNameGenerator
 
-
 class ClassNameAsBeanNameGenerator implements BeanNameGenerator {
     String generateBeanName(BeanDefinition beanDefinition, BeanDefinitionRegistry beanDefinitionRegistry) {
         beanDefinition.beanClassName
